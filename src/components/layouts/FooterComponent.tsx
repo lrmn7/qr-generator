2063d77d5731c749704f7fb5c60ba02f304e9282
L RMN <97253349+lrmn7@users.noreply.github.com>
2023-05-12 22:14:57 +0700

    Update FooterComponent.tsx

--- src/components/layouts/FooterComponent.tsx
+++ src/components/layouts/FooterComponent.tsx
@@ -1,34 +1,30 @@
 import React from "react";
 import { SiGithubsponsors } from "react-icons/si";
 
-import clsxm from "@/lib/helpers/clsxm";
+import clsx from "clsx";
 
-import BaseImage from "../images/BaseImage";
 import UnderlineLink from "../links/UnderlineLink";
-import UnstyledLink from "../links/UnstyledLink";
 
 const FooterComponent = () => {
   return (
     <footer
-      className={clsxm(
+      className={clsx(
         "mx-auto flex max-w-3xl flex-wrap items-center justify-center gap-2 md:justify-between",
         "px-2 py-16 md:px-1"
       )}
     >
       <div className="flex items-center gap-4">
-        <UnderlineLink href="https://is-a.fun">
-          {new Date().getFullYear()} | L RMN
-        </UnderlineLink>
+        {new Date().getFullYear()} | L RMN
       </div>
       <UnderlineLink
         href="https://github.com/sponsors/lrmn7"
-        className="group gap-2"
+        className="group gap-2 flex items-center"
       >
         <SiGithubsponsors
           size={24}
           className="text-darkpurple-400 group-hover:text-primary-500 dark:group-hover:text-primary-300"
         />
-        Support me through Github Sponsor
+        <span>Support me through Github Sponsor</span>
       </UnderlineLink>
     </footer>
   );
