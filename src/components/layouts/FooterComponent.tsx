c9a64a4679dc330cb2132918f151bf34ddb6a614
L RMN <97253349+lrmn7@users.noreply.github.com>
2023-05-12 10:37:07 +0700

    Add files via upload

--- src/components/layouts/FooterComponent.tsx
+++ src/components/layouts/FooterComponent.tsx
@@ -16,7 +16,6 @@ const FooterComponent = () => {
       )}
     >
       <div className="flex items-center gap-4">
-<<<<<<< HEAD
         <UnderlineLink href="https://lrmn.is-a.fun">
           {new Date().getFullYear()} | L RMN
         </UnderlineLink>
@@ -25,22 +24,9 @@ const FooterComponent = () => {
           target="_blank"
         >
           <BaseImage
-            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=370516&theme=light"
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=393953&theme=light"
             alt="QR&#0032;Generator - Simple&#0032;QR&#0032;Code&#0032;Generator&#0046; | Product Hunt"
             className="w-32 hover:brightness-75"
-=======
-        <UnderlineLink href="https://is-a.fun">
-          {new Date().getFullYear()} | L RMN
-        </UnderlineLink>
-        <UnstyledLink
-        href="https://www.producthunt.com/posts/qr-generator-3?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-qr&#0045;generator&#0045;3"
-          target="_blank"
-        >
-          <BaseImage
-            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=393953&theme=light"
-            alt="QR&#0032;Generator - simply&#0032;and&#0032;effortlessly&#0032;with&#0032;our&#0032;tool&#0046; | Product Hunt"
-            style="width: 250px; height: 54px;" width="250" height="54" /></a>
->>>>>>> eff4a04 (update embed)
           />
         </UnstyledLink>
       </div>
@@ -58,8 +44,4 @@ const FooterComponent = () => {
   );
 };
 
-<<<<<<< HEAD
 export default FooterComponent;
-=======
-export default FooterComponent;
->>>>>>> eff4a04 (update embed)
